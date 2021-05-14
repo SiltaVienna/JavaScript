5e14f3e909dc2a55d3ebfce46a7b04f4a2686e3f
SiltaVienna <82389641+SiltaVienna@users.noreply.github.com>
2021-05-14 14:43:45 +0200

    Add files via upload

---
+++ Opdracht1 JavaScript/index.js
@@ -0,0 +1,22 @@
+console.log("Hello Winc Academy");
+
+let firstName="Sil";
+console.log(firstName);
+
+let uitkomst= 4 + 4;
+console.log(uitkomst)
+
+let optellen="4" + "4";
+console.log(optellen);
+
+let substraction= 4 - 2;
+console.log(substraction);
+
+let multi = 4 * 4;
+console.log(multi);
+
+let division = 18 / 2;
+console.log(division);
+
+let age = 30;
+
