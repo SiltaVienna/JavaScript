1aeab4ffdfd9e52abcb314188b1b48b543ad2fb5
SiltaVienna <82389641+SiltaVienna@users.noreply.github.com>
2021-05-17 14:20:00 +0200

    Add files via upload

---
+++ Opdracht2 JavaScript/script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten")
+
+// Dit is een grote som
+let multi = 1230941 * 1823794;
+console.log(multi);
+
+/* 
+let division = 637263 / 54;
+console.log(division); */
