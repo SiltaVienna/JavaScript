fd890f5db7415b9dc32a2fdec8580bc1a8ab7ef7
SiltaVienna <82389641+SiltaVienna@users.noreply.github.com>
2021-05-17 17:26:55 +0200

    Add files via upload

---
+++ Opdracht4 JavaScript/script.js
@@ -0,0 +1,27 @@
+const age = 22;
+
+if (age >= 18) {
+    console.log("je mag naar binnen");
+} else if (age < 18) {
+    console.log("je mag niet naar binnen");
+} 
+
+if (age >= 18 && age <= 25) {
+    console.log ("Je krijgt 50% korting!");
+} else {
+    console.log ("Je krijgt geen korting.")
+}
+
+const firstName = Bram;
+
+if (firstName = Bram || firstName = Sarah) {
+    console.log ("Bram krijgt een gratis biertje!");
+} else {
+    console.log ("Jij krijgt geen gratis biertje");
+}
+
+const totalAmount = 25;
+
+if (totalAmount >25 || totalAmount >50) {
+    console.log 
+}
