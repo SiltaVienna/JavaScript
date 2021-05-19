c99f15dda41a0cbf3c58a1101af076af94b8d07f
SiltaVienna <82389641+SiltaVienna@users.noreply.github.com>
2021-05-19 16:10:11 +0200

    Add files via upload

---
+++ Opdracht6 Functions/script.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log("Make the dough");
+    console.log("Make the sauce");
+    console.log("Add cheese! More cheese is better");
+    console.log("Add toppings you like");
+    console.log("Put in oven");
+    console.log("Eat pizza!");
+}
+
+const makeSushi = function (){
+    console.log("Get some zeewier");
+    console.log("Add rice");
+    console.log("Add toppings");
+    console.log("Roll that sushi!");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
